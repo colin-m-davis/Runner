c1de97250e7847b8f1cdf6e42c66484d6175675b
colindavis <colinmichaelsdavis@gmail.com>
2022-07-01 10:03:10 -0700

    Initial Commit

---
+++ Runner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Runner
+//
+//  Created by Colin Davis on 7/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Runner/RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerApp.swift
+//  Runner
+//
+//  Created by Colin Davis on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
